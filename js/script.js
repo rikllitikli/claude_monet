9287642a1b5f1e5bce6e5a00aa2d5ddb9f14a79a
Kirill Pushkarow <kirrrilll98@gmail.com>
2021-09-06 12:59:56 +0300

    adaptive

--- js/script.js
+++ js/script.js
@@ -7,13 +7,27 @@ $(document).ready(function(){
         navText: [ '', ' ' ],
 
         responsive:{
+            1000:{
+                items:4
+            },
+            992:{
+                items:3
+            },
+            576:{
+                items:3
+            },
+            575:{
+                items:2
+            },
+            414:{
+                items:2
+            },
+            413:{
+                items:1
+            },
             0:{
                 items:1
             },
-
-            1000:{
-                items:1
-            }
         }
     });
 });
